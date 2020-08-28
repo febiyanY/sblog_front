15507991e36d28f9002f6a698e4ca16764bb394a
febiYanY <yfebiyan@gmail.com>
2020-08-28 09:22:26 +0700

    clean up ignored files

--- src/axioses/axios-default.js
+++ src/axioses/axios-default.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    // baseURL : process.env.REACT_APP_API_URL
-    baseURL : 'http://127.0.0.1:3001'
+    baseURL : process.env.REACT_APP_API_URL
 })
 
 export default instance
