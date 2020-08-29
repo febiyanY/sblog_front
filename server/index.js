e50a105fcb391943bd4e5afa0162f01a879e931c
febiYanY <yfebiyan@gmail.com>
2020-08-29 09:08:10 +0700

    fix port var

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
-const port = 3000 || process.env.port
+const port = 3000 || process.env.PORT
 const buildPath = path.join(__dirname, '../build')
 
 app.use(express.static(buildPath))
